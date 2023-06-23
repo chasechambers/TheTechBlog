585563760b97e68ed58bc4d8032dd49ed507852b
Chase <chambers.chase@gmail.com>
2023-06-23 14:17:28 -0400

    loop error

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,9 +2,12 @@ const router = require('express').Router();
 const BlogPost = require('../models/BlogPost');
 // const withAuth = require('../utils/auth');
 
-router.get('/', async (req, res) => {
-  const blogposts = await BlogPost.findAll();
-  console.log(blogposts);
+router.get('/', (req, res) => {
+  BlogPost.findAll()
+  .then (blogposts => {
+    console.log(blogposts)
+  })
+  
       // res.render('index', {
       //   index: blogpost
       // });
