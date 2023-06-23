748cb855ca88a7235acfa470007562903dbaae7d
Chase <chambers.chase@gmail.com>
2023-06-23 12:44:57 -0400

    Updating layouts

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const routes = require('./controllers');
 
 const db = require('./config/connection')
 
-
+// Use Public Folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Uses routes
