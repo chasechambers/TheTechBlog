9d40e1221fc97fdb0f8025b867fb694b69c6e0d2
Chase <chambers.chase@gmail.com>
2023-06-29 14:14:33 -0400

    tweaked model

--- models/index.js
+++ models/index.js
@@ -14,13 +14,13 @@ Blog.belongsTo(User, {
 
 
 
-User.hasMany(Comment, {
-  foreignKey: 'user_id',
+Blog.hasMany(Comment, {
+  foreignKey: 'blog_id',
   onDelete: 'CASCADE'
 });
 
-Comment.belongsTo(User, {
-  foreignKey: 'user_id'
+Comment.belongsTo(Blog, {
+  foreignKey: 'blog_id'
 });
 
 module.exports = { User, Blog, Comment };
